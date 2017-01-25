bfc37a887088095be6a85867ebed03787a7f4f2b
Maurits Ridder <maurits.ridder@ordina.nl>
2017-01-25 20:08:54 +0100

    Add working room

--- socket-examples/index.js
+++ socket-examples/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
@@ -7,10 +6,26 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-function onConnection(socket){
-  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+// Default namespace
+function onConnection(socket) {
+	// console.log(socket.id)
+	socket.broadcast.emit('newConnection', 'hi! Someone else has connected!');
+	socket.on('emitMsgBack', (data) => socket.emit('emitMsgBack', `This was your message: ${data}`));
+	socket.on('emitToOthers', (data) => socket.broadcast.emit('emitToOthers', `This was your message: ${data}`));
+	socket.on('disconnect', () => socket.broadcast.emit('disconnectedClient', 'hi! Someone has disconnected!'))
 }
-
 io.on('connection', onConnection);
+
+// Custom namespace
+function onVIPConnection(socket) {
+	socket.broadcast.emit('newVIP', 'VIP!');
+	socket.on('joinAdminVIP', () => {
+		socket.join('VIPAdmin');
+		socket.to('VIPAdmin').emit('adminJoined', 'EMIT: new person has joined admin room in VIP namespace');
+	});
+}
+const VIP = io.of('/VIP'); // Declare the custom namespace (unrelated to the URL)
+VIP.on('connection', onVIPConnection);
+
 
 http.listen(port, () => console.log('listening on port ' + port));
--- socket-examples/public/main.js
+++ socket-examples/public/main.js
@@ -3,11 +3,49 @@
 (function () {
 
 	var socket = io();
+	var VIPSocket;
 
-	const emitToOthers = () => {
-		console.log('emittt');
-	};
-	const emit1 = document.getElementById('socket1');
-	emit1.addEventListener('click', emitToOthers, false);
+	socket.on('newConnection', (data) => {
+		console.log(data);
+	});
 
+	// console.log(socket)
+
+	socket.on('disconnectedClient', (data) => {
+		console.log(data);
+	});
+
+	const emit1 = document.getElementById('emitMsgBack');
+	emit1.addEventListener('click', () => {
+		socket.emit('emitMsgBack', 'foo')
+	}, false);
+	socket.on('emitMsgBack', (msg) => {
+		console.log(msg);
+	});
+
+	const emit2 = document.getElementById('emitToOthers');
+	emit2.addEventListener('click', () => {
+		socket.emit('emitToOthers', 'coolbeans')
+	}, false);
+	socket.on('emitToOthers', (msg) => {
+		console.log(msg);
+	});
+
+	const emit3 = document.getElementById('connectToVIP');
+	emit3.addEventListener('click', () => {
+		VIPSocket = io('/VIP');
+		VIPSocket.on('newVIP', (msg) => {
+			console.log('a new VIP has connected', msg)
+		});
+		emit3.setAttribute('disabled', 'disabled');
+		emit4.removeAttribute('disabled');
+	}, false);
+
+	const emit4 = document.getElementById('connectToAdminVIP');
+	emit4.addEventListener('click', () => {
+		VIPSocket.emit('joinAdminVIP');
+		VIPSocket.on('adminJoined', (msg) => {
+			console.log(msg);
+		});
+	}, false);
 })();
